0afb6bd96a1bd77eb29c97b39df93688ba5050c6
Hiruna Jayamanne <hirunav2@gmail.com>
2018-07-02 22:03:48 +1200

    Added all the assets into the repo

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,55 @@
-extern crate svnf;
+extern crate gobu;
 extern crate piston_window;
 extern crate gfx_glyph;
 
-use std::sync::Arc;
-use svnf::{
-    game::*,
+use gobu::{
+    GameBuilder,
     util::GfxGlyph,
 };
 use piston_window::{
     PistonWindow,
     WindowSettings,
-    TextureSettings,
-    Texture,
-    Flip,
+};
+use std::{
+    fs::File,
+    io::Read,
 };
 use gfx_glyph::GlyphBrushBuilder;
-const WIDTH: u32 = 800;
+const WIDTH: u32 = 640;
 const HEIGHT: u32 = 560;
 
 fn main() {
-        let mut window: PistonWindow = WindowSettings::new("Hello, World!", [WIDTH, HEIGHT])
-            .exit_on_esc(true)
-            .build()
-            .expect("Error in creating the window!");
-        let mut game = Game::new([WIDTH as f64, HEIGHT as f64]);
-        let background = Arc::new(Texture::from_path(
-            &mut window.factory,
-            "../assets/megumin.jpg",
-            Flip::None,
-            &TextureSettings::new())
-            .expect("Error in creating the background texture!"));
-        game.add_background("background1".to_string(),background.clone());
-        game.load_characters_from_file("./assets/characters.toml", &mut window.factory)
-            .expect("Error in loading the characters!");
-        game.story.load_from_file("main".to_string(), "./assets/scripts/script.txt")
-            .expect("Error in loading the script from the string");
-        game.story.set_script("main", None);
-        game.apply_grid(32, 32);
-        game.load_gui_from_file("./assets/gui.toml")
-            .expect("Error in loading the GUI file!");
-        game.load_input_from_file("./assets/input.toml")
-            .expect("Error in loading the Input file!");
-        let font: &[u8] = include_bytes!("C:\\Users\\Hiruna\\Documents\\Projects\\Rust\\vn\\example_vn\\assets\\HanaMinA.ttf");
-        let mut brush = GlyphBrushBuilder::using_font_bytes(font)
-            .build(window.factory.clone());
-        while let Some(event) = window.next() {
-            game.handle_event(&event);
-            use piston_window::{Event, Loop};
-            if let Event::Loop(Loop::Render(_)) = event {
-                game.draw_text(&mut brush);
-                window.draw_2d_with_text( & event, &mut brush, | c, g |{
-                    &game.draw(c, g);
-                }).expect("Error in writing the text!");
-            }
+    let mut window: PistonWindow = WindowSettings::new("Hello, World!", [WIDTH, HEIGHT])
+        .exit_on_esc(true)
+        .build()
+        .expect("Error in creating the window!");
+    let mut game = GameBuilder::new([WIDTH as f64, HEIGHT as f64])
+        .characters("./assets/config/characters.toml")
+        .grid(32, 32)
+        .gui("./assets/config/gui.toml")
+        .input("./assets/config/input.toml")
+        .backgrounds("./assets/config/background.toml")
+        .scripts("./assets/config/scripts.toml")
+        .build(&mut window.factory)
+        .expect("Error in building the game!");
+    let font = {
+        let mut buffer = vec![];
+        File::open("./assets/fonts/HanaMinA.ttf")
+            .expect("Error finding font!")
+            .read_to_end(&mut buffer)
+            .expect("Error reading font!");
+        buffer
+    };
+    let mut brush = GlyphBrushBuilder::using_font_bytes(font.as_slice())
+        .build(window.factory.clone());
+    while let Some(event) = window.next() {
+        game.handle_event(&event);
+        use piston_window::{Event, Loop};
+        if let Event::Loop(Loop::Render(_)) = event {
+            game.draw_text(&mut brush);
+            window.draw_2d_with_text( & event, &mut brush, | c, g |{
+                &game.draw(c, g);
+            }).expect("Error in writing the text!");
         }
+    }
 }
